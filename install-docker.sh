f7fc6aa74d8bf9c8903081f2e97aae112c522de9
Samir LAKHDARI <samir.lakhdari@mac.com>
2022-01-02 09:37:15 +0100

    first commit

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum-config-manager  --add-repo   https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker vagrant
